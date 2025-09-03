3c1d8ead1a70fb96f8621b1f1ad430b955850edd
Romil <romil.raja@bwispl.in>
2025-09-03 11:18:13 +0530

    Updated

--- example_1.py
+++ example_1.py
@@ -28,20 +28,23 @@
 # print("romil","Ahmedabad",sep="###",end="100")
 # print("380002","parimal-garden",sep="$$")
 
-name="Tops\tTechnologies"
-print(name)
-name="Tops\bTechnologies"
-print(name)
-print("tops","technogies",sep="\n")
-print("tops","technogies",sep="\t")
-print("tops technogies",end="\n")
-print("tops technogies",end="\t")
+# name="Tops\tTechnologies"
+# print(name)
+# name="Tops\bTechnologies"
+# print(name)
+# print("tops","technogies",sep="\n")
+# print("tops","technogies",sep="\t")
+# print("tops technogies",end="\n")
+# print("tops technogies",end="\t")
 
-name="Romil\tRaja"
-print(name)
-name="Romil\bRaja"
-print(name)
-print("Romil","Raja",sep="\n")
-print("Romil","Raja",sep="\t")
-print("Romil Raja",end="\n")
-print("Romil Raja",end="\t")
+# name="Romil\tRaja"
+# print(name)
+# name="Romil\bRaja"
+# print(name)
+# print("Romil","Raja",sep="\n")
+# print("Romil","Raja",sep="\t")
+# print("Romil Raja",end="\n")
+# print("Romil Raja",end="\t")
+
+a = input("Enter value: ")
+print(f"{a} * 10 is {a * 10}")
